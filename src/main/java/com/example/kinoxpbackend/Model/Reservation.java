cb7efafc7d0f47de1d907f9d4fc55308561ba391
Niinyah <petermarbell@gmail.com>
2026-03-03 12:47:03 +0100

    Reservation and movie model

--- src/main/java/com/example/kinoxpbackend/Model/Reservation.java
+++ src/main/java/com/example/kinoxpbackend/Model/Reservation.java
@@ -1,4 +1,89 @@
 package com.example.kinoxpbackend.Model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String customerName;
+    private String customerEmail;
+    private LocalDateTime creationDate;
+    private double price;
+
+    @ManyToOne
+    private Screening screening;
+
+    @OneToMany(cascade = {CascadeType.REMOVE, CascadeType.PERSIST} )
+    private List<Seat> seats = new ArrayList<>();
+
+    public Reservation() {}
+
+    public Reservation(String customerName, String customerEmail, LocalDateTime creationDate, double price) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.creationDate = creationDate;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Screening getScreening() {
+        return screening;
+    }
+
+    public void setScreening(Screening screening) {
+        this.screening = screening;
+    }
+
+    public List<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<Seat> seats) {
+        this.seats = seats;
+    }
 }
