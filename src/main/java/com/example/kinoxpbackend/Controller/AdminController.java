be23913115bf262236329269336be6a3f03cb5ab
Niinyah <petermarbell@gmail.com>
2026-03-04 14:02:23 +0100

    Broken code

--- src/main/java/com/example/kinoxpbackend/Controller/AdminController.java
+++ src/main/java/com/example/kinoxpbackend/Controller/AdminController.java
@@ -1,4 +1,46 @@
 package com.example.kinoxpbackend.Controller;
 
+
+import com.example.kinoxpbackend.Service.AdminService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<Void> login(@RequestParam String username, @RequestParam String pw, HttpSession session) {
+        Admin admin = adminService.getAdmin(username, pw);
+        if (admin != null) {
+            session.setAttribute("admin", true);
+        }
+        return ResponseEntity.ok().build();
+    }
+
 }
+
+@PostMapping("/logout")
+public ResponseEntity<Void> logout(HttpSession session) {
+    session.invalidate();
+    return ResponseEntity.noContent().build();
+}
+
+@GetMapping("/session")
+public ResponseEntity<Map<String, Boolean>> checkSession(HttpSession session) {
+    boolean active = session.getAttribute("admin") != null;
+    return ResponseEntity.ok(Map.of("active", active));
+}
+
+
---
+++ src/main/java/com/example/kinoxpbackend/Exception/NotfoundException.java
@@ -0,0 +1,8 @@
+package com.example.kinoxpbackend.Exception;
+
+public class NotfoundException extends RuntimeException {
+    public NotfoundException(String message) {
+        super(message);
+
+    }
+}
