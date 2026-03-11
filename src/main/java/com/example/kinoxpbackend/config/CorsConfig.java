128e5f00dff63443540e41ffbdc8b8aef873527b
Niinyah <petermarbell@gmail.com>
2026-03-11 09:19:53 +0100

    cors-config

--- src/main/java/com/example/kinoxpbackend/config/CorsConfig.java
+++ src/main/java/com/example/kinoxpbackend/config/CorsConfig.java
@@ -14,9 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://127.0.0.1:5501", "http://localhost:5501")
                         .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+
             }
         };
     }
